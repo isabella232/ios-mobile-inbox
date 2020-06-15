104277a859fbcb21d99e097639477199a04dc19a
eduzatoni <eduzatoni@outlook.com>
2020-06-15 17:09:19 +0100

    xcode project created

---
+++ InboxSample/InboxSample/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2019 Emarsys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
